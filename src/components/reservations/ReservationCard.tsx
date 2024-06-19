ef9aad13f7258de16198641100897aff8dd7bc76
zana-abdollahpour <zana.ap4@gmail.com>
2024-06-19 16:18:04 +0330

    fix type issues in ReservationCard and database.types

--- src/components/reservations/ReservationCard.tsx
+++ src/components/reservations/ReservationCard.tsx
@@ -33,7 +33,7 @@ function ReservationCard({ booking }: ReservationCardProps) {
     <div className="flex border border-primary-800">
       <div className="relative aspect-square h-32">
         <Image
-          src={image}
+          src={image!}
           alt={`Cabin ${name}`}
           className="border-r border-primary-800 object-cover"
         />
@@ -44,7 +44,7 @@ function ReservationCard({ booking }: ReservationCardProps) {
           <h3 className="text-xl font-semibold">
             {numNights} nights in Cabin {name}
           </h3>
-          {isPast(new Date(startDate)) ? (
+          {isPast(new Date(startDate!)) ? (
             <span className="flex h-7 items-center rounded-sm bg-yellow-800 px-3 text-xs font-bold uppercase text-yellow-200">
               past
             </span>
@@ -56,21 +56,21 @@ function ReservationCard({ booking }: ReservationCardProps) {
         </div>
 
         <p className="text-lg text-primary-300">
-          {format(new Date(startDate), "EEE, MMM dd yyyy")} (
-          {isToday(new Date(startDate))
+          {format(new Date(startDate!), "EEE, MMM dd yyyy")} (
+          {isToday(new Date(startDate!))
             ? "Today"
-            : formatDistanceFromNow(startDate)}
-          ) &mdash; {format(new Date(endDate), "EEE, MMM dd yyyy")}
+            : formatDistanceFromNow(startDate!)}
+          ) &mdash; {format(new Date(endDate!), "EEE, MMM dd yyyy")}
         </p>
 
         <div className="mt-auto flex items-baseline gap-5">
           <p className="text-xl font-semibold text-accent-400">${totalPrice}</p>
           <p className="text-primary-300">&bull;</p>
           <p className="text-lg text-primary-300">
-            {numGuests} guest{numGuests > 1 && "s"}
+            {numGuests} guest{numGuests! > 1 && "s"}
           </p>
           <p className="ml-auto text-sm text-primary-400">
-            Booked {format(new Date(created_at), "EEE, MMM dd yyyy, p")}
+            Booked {format(new Date(created_at!), "EEE, MMM dd yyyy, p")}
           </p>
         </div>
       </div>
