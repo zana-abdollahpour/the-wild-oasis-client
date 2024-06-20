0472e7bdeb90f2c5a454e3a020956b58c17acf33
zana-abdollahpour <zana.ap4@gmail.com>
2024-06-20 17:19:02 +0330

    highlight current link in account side navigation

--- src/components/side-navigation/SideNavigation.tsx
+++ src/components/side-navigation/SideNavigation.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import {
   CalendarDaysIcon,
   HomeIcon,
@@ -27,13 +30,15 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+  const pathName = usePathname();
+
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex h-full flex-col gap-2 text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`flex items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-colors hover:bg-primary-900 hover:text-primary-100`}
+              className={`flex items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-all hover:bg-primary-900 hover:text-primary-100 ${pathName === link.url ? "bg-primary-900" : ""}`}
               href={link.url}
             >
               {link.icon}
