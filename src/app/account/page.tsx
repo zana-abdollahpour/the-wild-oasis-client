f3c1844c8ec95bde9a56dc67d404ddbf2de329d7
zana-abdollahpour <zana.ap4@gmail.com>
2024-06-22 18:07:54 +0330

    show logged in username in account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,13 +1,18 @@
 import type { Metadata } from "next";
 
+import { auth } from "@/utils/auth";
+
 export const metadata: Metadata = {
   title: "Guest Area",
 };
 
-export default function AccountPage() {
+export default async function AccountPage() {
+  const session = await auth();
+  const firstName = session?.user?.name!.split(" ")[0]!;
+
   return (
     <h2 className="mb-7 text-2xl font-semibold text-accent-400">
-      Welcome, Zana
+      Welcome, {firstName}
     </h2>
   );
 }
