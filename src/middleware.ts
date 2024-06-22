2764ba116b47033fb472e60ead85dd27d2d5c99d
zana-abdollahpour <zana.ap4@gmail.com>
2024-06-22 18:31:26 +0330

    fix matcher problem in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
 import type { MiddlewareConfig } from "next/server";
 
-import { mainRoutes } from "./routes";
 import { auth } from "./utils/auth";
 
 export const middleware = auth;
 
 export const config: MiddlewareConfig = {
-  matcher: [mainRoutes.account.url],
+  matcher: ["/account"],
 };
